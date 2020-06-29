ddc97f0abe31b9d022dc5cffb815736179254831
JohnK-mi <56094449+JohnK-mi@users.noreply.github.com>
2020-06-28 21:16:00 -0700

    Initial File

---
+++ pixelForm.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+
+int main(int argc char *argv[]){
+    if(argc!=4){
+        std::cerr<<"ERROR! Correct usage is ./pixelForm fromFile toFile isLast"
+    }
+    else{
+        ofstream myfile.open(argv[2]);
+        ifstream frofile.open(argv[1]);
+        getline.(frofile,myText);
+        myfile<<mytext;
+        if(argv[3]=="y"||argv[3]=="Y"){
+            myfile<<"],"
+        }
+        else{
+            myfile<<"]}"
+        }
+        
+    }
+
+}
