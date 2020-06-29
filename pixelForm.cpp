be814567845de385ab17c4ea3cb83af2ef22366a
JohnK-mi <56094449+JohnK-mi@users.noreply.github.com>
2020-06-28 21:32:26 -0700

    Update pixelForm.cpp

--- pixelForm.cpp
+++ pixelForm.cpp
@@ -2,20 +2,21 @@
 #include <fstream>
 using namespace std;
 int main(int argc, char *argv[]){
-    if(argc!=4){
-        cerr<<"ERROR! Correct usage is ./pixelForm fromFile toFile isLast";
+    if(argc!=5){
+        cerr<<"ERROR! Correct usage is ./pixelForm fromFile toFile isLast letter";
     }
     else{
         ofstream myfile(argv[2]);
         ifstream frofile(argv[1]);
         string myText;
         getline(frofile,myText);
+        myFile<<"\""<<argv[4]<<"\":"
         myfile<<myText;
         if(argv[3]=="y"||argv[3]=="Y"){
-            myfile<<"],";
+            myfile<<"}";
         }
         else{
-            myfile<<"]}";
+            myfile<<",";
         }
         
     }
